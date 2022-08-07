5c27c563311a25080e4088739d8f25263ff60ca8
Wan2302 <erwan.cherel35@hotmail.com>
2022-08-07 10:20:28 +0200

    blocked always display you loose

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let choice = ["Rock", "Paper", "Scissors"];
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
+  const choice = ["Rock", "Paper", "Scissors"];
   return randomChoice = choice[Math.floor(Math.random() * choice.length)];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+    return "Tie!";
+  } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+             (playerSelection == "paper" && computerSelection == "rock") ||
+             (playerSelection == "scissors" && computerSelection == "paper")) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
