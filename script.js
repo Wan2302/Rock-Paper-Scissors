340c321855d8b3e4cf5e93f5b3904c5dfc409313
Wan2302 <erwan.cherel35@hotmail.com>
2022-08-07 11:58:49 +0200

    return a result; next step: add score

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const playerSelection = "paper";
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
-  const choice = ["Rock", "Paper", "Scissors"];
-  return randomChoice = choice[Math.floor(Math.random() * choice.length)];
+  const choice = ["rock", "paper", "scissors"];
+  return choice[Math.floor(Math.random() * choice.length)];
+  
+}
+
+function getPlayerChoice() {
+  const choice = prompt("Rock, paper, scissors?");
+  playerChoice = choice.toLowerCase();
+  return playerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+    if (playerSelection === computerSelection) {
     return "Tie!";
-  } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
-             (playerSelection == "paper" && computerSelection == "rock") ||
-             (playerSelection == "scissors" && computerSelection == "paper")) {
+  } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+             (playerSelection === "paper" && computerSelection === "rock") ||
+             (playerSelection === "scissors" && computerSelection === "paper")) {
     return `You win! ${playerSelection} beats ${computerSelection}.`;
   } else {
     return `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
 }
 
+function game() {
+  
+}
 
+console.log(playerSelection);
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
